dd86e431d772cbd40cb723a340bac79fbe143266
benjaminhao <benjaminhao@dreawer.com>
2023-04-27 14:51:32 +0800

    feat: if code

--- src/compile/tokens/programming/typescript/core/__test__/fixtures/stmtIf.ts
+++ src/compile/tokens/programming/typescript/core/__test__/fixtures/stmtIf.ts
@@ -1,27 +1,141 @@
 import { Statement } from '../../../../types/statement';
 import { mock_declare_type_value } from './stmtDeclare';
-import { expression_call, expression_call1 } from './stmtExprCall';
-import { expression_identifier1 } from './stmtExprIdentifier';
 
 export const mock_if: Statement.If = {
   _statement_: 'if',
-  ifs: [[expression_identifier1, []]],
+  ifs: [
+    [
+      {
+        _statement_: 'expression',
+        _expression_: 'identifier',
+        escapedText: 'temp',
+      },
+      [],
+    ],
+  ],
 };
 export const mock_if_else: Statement.If = {
   _statement_: 'if',
-  ifs: [[expression_identifier1, []]],
+  ifs: [
+    [
+      {
+        _statement_: 'expression',
+        _expression_: 'identifier',
+        escapedText: 'temp',
+      },
+      [],
+    ],
+  ],
   else: [],
 };
 export const mock_if_elseIf: Statement.If = {
   _statement_: 'if',
   ifs: [
-    [expression_identifier1, []],
-    [expression_call, []],
+    [
+      {
+        _statement_: 'expression',
+        _expression_: 'identifier',
+        escapedText: 'temp',
+      },
+      [],
+    ],
+    [
+      {
+        _statement_: 'expression',
+        _expression_: 'identifier',
+        escapedText: 'temp1',
+      },
+      [],
+    ],
+    [
+      {
+        _statement_: 'expression',
+        _expression_: 'identifier',
+        escapedText: 'temp2',
+      },
+      [],
+    ],
   ],
   else: [],
 };
 export const mock_if_elseIf_statement: Statement.If = {
   _statement_: 'if',
-  ifs: [[expression_identifier1, [expression_call, mock_if_else]]],
-  else: [mock_declare_type_value, expression_call1],
+  ifs: [
+    [
+      {
+        _statement_: 'expression',
+        _expression_: 'identifier',
+        escapedText: 'temp',
+      },
+      [
+        {
+          _statement_: 'expression',
+          _expression_: 'call',
+          expression: {
+            _statement_: 'expression',
+            _expression_: 'access',
+            expression: {
+              _statement_: 'expression',
+              _expression_: 'identifier',
+              escapedText: 'console',
+            },
+            name: {
+              _statement_: 'expression',
+              _expression_: 'identifier',
+              escapedText: 'log',
+            },
+          },
+          args: [
+            {
+              _statement_: 'expression',
+              _expression_: 'literal',
+              type: 'int',
+              value: 1,
+            },
+          ],
+        },
+      ],
+    ],
+  ],
+  else: [
+    {
+      _statement_: 'declare',
+      name: {
+        _statement_: 'expression',
+        _expression_: 'identifier',
+        escapedText: 'title',
+      },
+      value: {
+        _statement_: 'expression',
+        _expression_: 'literal',
+        type: 'string',
+        value: '中国',
+      },
+    },
+    {
+      _statement_: 'expression',
+      _expression_: 'call',
+      expression: {
+        _statement_: 'expression',
+        _expression_: 'access',
+        expression: {
+          _statement_: 'expression',
+          _expression_: 'identifier',
+          escapedText: 'console',
+        },
+        name: {
+          _statement_: 'expression',
+          _expression_: 'identifier',
+          escapedText: 'log',
+        },
+      },
+      args: [
+        {
+          _statement_: 'expression',
+          _expression_: 'identifier',
+          escapedText: 'title',
+        },
+      ],
+    },
+  ],
 };
--- src/compile/tokens/programming/typescript/core/__test__/stmtIf.test.ts
+++ src/compile/tokens/programming/typescript/core/__test__/stmtIf.test.ts
@@ -1,24 +1,22 @@
 import { describe, it, expect } from 'vitest';
 import { statement } from '../statement';
-import {
-  mock_if, mock_if_else, mock_if_elseIf, mock_if_elseIf_statement
-} from './fixtures/stmtIf';
+import { mock_if, mock_if_else, mock_if_elseIf, mock_if_elseIf_statement } from './fixtures/stmtIf';
 
 describe('statement if', () => {
   it('if', () => {
     const code = statement.if(mock_if);
-    expect(`if(identifierText){}`).toEqual(code);
+    expect(`if(temp){}`).toEqual(code);
   });
   it('if else', () => {
     const code = statement.if(mock_if_else);
-    expect(`if(identifierText){}else{}`).toEqual(code);
+    expect(`if(temp){}else{}`).toEqual(code);
   });
   it('if elseIf else', () => {
     const code = statement.if(mock_if_elseIf);
-    expect(`if(identifierText){}else if(func('abc',200,true)){}else{}`).toEqual(code);
+    expect(`if(temp){}else if(temp1){}else if(temp2){}else{}`).toEqual(code);
   });
   it('if elseIf else statement', () => {
     const code = statement.if(mock_if_elseIf_statement);
-    expect(`if(identifierText){func('abc',200,true);if(identifierText){}else{};}else{let title:string='中国';tools.isString();}`).toEqual(code);
+    expect(`if(temp){console.log(1);}else{let title='中国';console.log(title);}`).toEqual(code);
   });
 });
