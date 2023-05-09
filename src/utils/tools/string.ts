7230a53b78d687289679905754b6b670439dc792
james <james@dreawer.com>
2023-05-09 18:34:53 +0800

    feat: 转驼峰的方法优化

--- src/utils/tools/string.ts
+++ src/utils/tools/string.ts
@@ -8,14 +8,21 @@ export const string = {
   firstWordLowerCase: (str = '') => {
     return str.substring(0, 1).toLowerCase() + str.substring(1);
   },
-  // 线转换驼峰
-  lineToHump: (str = '', line = '-') => {
+  // 线转换大驼峰
+  lineToHumpBig: (str = '', line = '-') => {
     const reg = new RegExp(line + '(\\w)', 'gim');
     return string.firstWordUpperCase(
       str.replace(reg, function (all, letter, a) {
         return letter.toUpperCase();
       })
     );
+  },
+  // 线转换驼峰
+  lineToHump: (str = '', line = '-') => {
+    const reg = new RegExp(line + '(\\w)', 'gim');
+    return str.replace(reg, function (all, letter, a) {
+      return letter.toUpperCase();
+    });
   },
   // 驼峰转换线
   humpToLine: (str = '', line = '-') => {
