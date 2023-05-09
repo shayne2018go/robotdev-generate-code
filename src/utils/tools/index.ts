a13e056100bb94af16e2d5e6ac025595c410d3ca
james <james@dreawer.com>
2023-05-09 17:58:07 +0800

    feat: 新增帮助工具

--- src/utils/tools/index.ts
+++ src/utils/tools/index.ts
@@ -1,6 +1,7 @@
 import { dataType } from './dataType';
-
+import { string } from './string';
 export { dataType };
 export const tools = {
   dataType,
+  string,
 };
---
+++ src/utils/tools/string.ts
@@ -0,0 +1,28 @@
+export const string = {
+  // 首字母大写
+  firstWordUpperCase: (str = '') => {
+    return str.substring(0, 1).toUpperCase() + str.substring(1);
+  },
+
+  // 首字母小写
+  firstWordLowerCase: (str = '') => {
+    return str.substring(0, 1).toLowerCase() + str.substring(1);
+  },
+  // 线转换驼峰
+  lineToHump: (str = '', line = '-') => {
+    const reg = new RegExp(line + '(\\w)', 'gim');
+    return string.firstWordUpperCase(
+      str.replace(reg, function (all, letter, a) {
+        return letter.toUpperCase();
+      })
+    );
+  },
+  // 驼峰转换线
+  humpToLine: (str = '', line = '-') => {
+    const reg = new RegExp('^' + line, 'gim');
+    return str
+      .replace(/([A-Z])/g, line + '$1')
+      .toLowerCase()
+      .replace(reg, '');
+  },
+};
