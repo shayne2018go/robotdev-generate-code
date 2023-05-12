b8625db5658758bd48c5ac3cae514144a9543ee9
benjaminhao <benjaminhao@dreawer.com>
2023-05-12 08:42:00 +0800

    feat: 修改script

---
+++ src/compile/syntax/vue/sfc/__test__/compileScript.test.ts
@@ -0,0 +1,26 @@
+import { describe, expect, it } from 'vitest';
+import compileScript from '../compileScript';
+import { parsingVueCompileOptions, buildGlobalCtx } from '../../compileVue';
+import codeSchema from '@/__test__/__fixture__/CodeSchema';
+import { CompilePageCtx, parsingPage } from '../../compilePages';
+import scriptToken from './__fixture__/script-tokens.json';
+
+describe('compileScript', () => {
+  // 解析相关依赖协议
+  const vueCompileOptions = parsingVueCompileOptions(codeSchema);
+
+  // 构建全局上下文
+  const vueGlobalCtx = buildGlobalCtx(vueCompileOptions);
+
+  const page = codeSchema.pages[3];
+
+  const parsingPageResult = parsingPage(page, vueGlobalCtx);
+
+  const currentPageCompileOptions: CompilePageCtx = Object.assign({}, vueGlobalCtx, parsingPageResult);
+
+  it('script', () => {
+    const { token } = compileScript(page, currentPageCompileOptions);
+    expect(token).toMatchSnapshot();
+    // expect(token).toEqual(scriptToken);
+  });
+});
