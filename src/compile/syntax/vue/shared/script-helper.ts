a9d2fc3a43a92c60d428675c238ce1543db67c76
benjaminhao <benjaminhao@dreawer.com>
2023-05-16 17:28:40 +0800

    feat: 排除slot和each

--- src/compile/syntax/vue/shared/script-helper.ts
+++ src/compile/syntax/vue/shared/script-helper.ts
@@ -1,11 +1,19 @@
 import { CompilePageCtx } from '../compilePages';
+import { VirtualTag } from '../const/config';
 
 export const getVariableVarName = (variableId: string, ctx: CompilePageCtx): string | undefined => {
   return ctx.scope.current.variablesStore.findId(variableId)?.varName;
 };
 
 export const getNodeTagVarName = (nodeId: string, ctx: CompilePageCtx): string | undefined => {
-  return ctx.scope.current.nodesStore.find(nodeId)?.varName;
+  const node = ctx.scope.current.nodesStore.find(nodeId);
+  const tag = node?.data.tagId && ctx.global.componentsStore.getCmpt(node?.data.tagId)?.key;
+  if (tag === VirtualTag.EACH) {
+    return undefined;
+  } else if (tag === VirtualTag.SLOT) {
+    return undefined;
+  }
+  return node?.varName;
 };
 
 export const getNodePropKeyByNodeId = (nodeId: string, propId: string, ctx: CompilePageCtx): string | undefined => {
