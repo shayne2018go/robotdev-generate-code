7d93f5a6f50340058e99255e7495a831e0e2e7c4
benjaminhao <benjaminhao@dreawer.com>
2023-05-16 15:08:51 +0800

    feat: 测试用例

--- src/compile/syntax/vue/shared/__test__/bind-parse/core.test.ts
+++ src/compile/syntax/vue/shared/__test__/bind-parse/core.test.ts
@@ -1,9 +1,72 @@
-import { describe } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { buildGlobalCtx, parsingVueCompileOptions } from '../../../compileVue';
 import codeSchema from '@/__test__/__fixture__/CodeSchema';
+import { CompilePageCtx, parsingCurrent } from '../../../compilePages';
+import { BindParseCtx, BindRdData } from '../../bind-parse/types';
+import { genVarName } from '../../helper';
+import { bindToAst } from '../../bind-parse/core';
+import generate from '@babel/generator';
 
 describe('core', () => {
+  // 解析相关依赖协议
   const vueCompileOptions = parsingVueCompileOptions(codeSchema);
 
+  // 构建全局上下文
   const vueGlobalCtx = buildGlobalCtx(vueCompileOptions);
+
+  const page = codeSchema.pages[0];
+
+  const parsingPageResult = parsingCurrent(page, vueGlobalCtx);
+
+  const currentPageCompileOptions: CompilePageCtx = {
+    global: vueGlobalCtx,
+    scope: {
+      current: parsingPageResult,
+    },
+  };
+  describe('bindToAst', () => {
+    const bindParseCtx: BindParseCtx = Object.assign(currentPageCompileOptions, {
+      scope: {
+        ...currentPageCompileOptions.scope,
+        ...{
+          actions: {
+            genVarName: genVarName(),
+            map: {},
+          },
+        },
+      },
+      helper: {
+        uniqueVarname: genVarName(),
+      },
+    });
+    it('getVar', () => {
+      bindParseCtx.scope.node = bindParseCtx.scope.current.nodesStore.getNode('Y576');
+      const ast = bindToAst(bindParseCtx.scope.node?.props?.[0].value! as BindRdData, bindParseCtx);
+      expect(generate(ast!).code).toEqual('variables.xunhuancols');
+    });
+    it('getApiData', () => {
+      bindParseCtx.scope.node = bindParseCtx.scope.current.nodesStore.getNode('F104');
+      const ast = bindToAst(bindParseCtx.scope.node?.props?.[0].value! as BindRdData, bindParseCtx);
+      expect(generate(ast!).code).toEqual('apiState.chaxunsuoyouyonghu.data.yonghushuzu');
+    });
+    it('getParam', () => {
+      bindParseCtx.scope.node = bindParseCtx.scope.current.nodesStore.getNode('F104');
+      const ast = bindToAst(bindParseCtx.scope.node?.props?.[1].value! as BindRdData, bindParseCtx);
+      expect(generate(ast!).code).toEqual('router.query.id');
+    });
+    it('getEventData', () => {
+      bindParseCtx.scope.node = bindParseCtx.scope.current.nodesStore.getNode('F104');
+      bindParseCtx.scope.event = bindParseCtx.scope.node?.events?.[0];
+      const ast = bindToAst(
+        (bindParseCtx.scope.node?.events?.[0].actions?.[0].args?.actions as CodeSchema.Action[])[0].args
+          .value! as BindRdData,
+        bindParseCtx
+      );
+      expect(generate(ast!).code).toEqual('event.target.value');
+    });
+    it('getSlotData', () => {});
+    it('getEachData', () => {});
+    it('getCmptPropData', () => {});
+    it('getArguments', () => {});
+  });
 });
---
+++ src/compile/syntax/vue/shared/__test__/helper/helper.test.ts
@@ -0,0 +1,68 @@
+import { describe, expect, it } from 'vitest';
+import { parsingVueCompileOptions, buildGlobalCtx } from '../../../compileVue';
+import codeSchema from '@/__test__/__fixture__/CodeSchema';
+import { CompilePageCtx, parsingCurrent } from '../../../compilePages';
+import {
+  getNodeEventKeyByNodeId,
+  getNodePropKeyByNodeId,
+  getNodeTagVarName,
+  getVariableVarName,
+} from '../../script-helper';
+
+describe('compileScript', () => {
+  describe('script-helper', () => {
+    // 解析相关依赖协议
+    const vueCompileOptions = parsingVueCompileOptions(codeSchema);
+
+    // 构建全局上下文
+    const vueGlobalCtx = buildGlobalCtx(vueCompileOptions);
+
+    const page = codeSchema.pages[0];
+
+    const parsingPageResult = parsingCurrent(page, vueGlobalCtx);
+
+    const currentPageCompileOptions: CompilePageCtx = {
+      global: vueGlobalCtx,
+      scope: {
+        current: parsingPageResult,
+      },
+    };
+
+    it('getVariableVarName', () => {
+      const str = getVariableVarName('6447262e0b661be9e0b91ac1', currentPageCompileOptions);
+      expect(str).toEqual('xueshengmingcheng');
+    });
+    it('getNodeTagVarName', () => {
+      const str = getNodeTagVarName('D767', currentPageCompileOptions);
+      const str1 = getNodeTagVarName('Y576', currentPageCompileOptions);
+      expect(str).toEqual('each');
+      expect(str1).toEqual('each_2');
+    });
+  });
+  describe('script-helper1', () => {
+    // 解析相关依赖协议
+    const vueCompileOptions = parsingVueCompileOptions(codeSchema);
+
+    // 构建全局上下文
+    const vueGlobalCtx = buildGlobalCtx(vueCompileOptions);
+
+    const page = codeSchema.pages[3];
+
+    const parsingPageResult = parsingCurrent(page, vueGlobalCtx);
+
+    const currentPageCompileOptions: CompilePageCtx = {
+      global: vueGlobalCtx,
+      scope: {
+        current: parsingPageResult,
+      },
+    };
+    it('getNodePropKeyByNodeId', () => {
+      const key = getNodePropKeyByNodeId('N735', '641a7052cf44f6204642b875', currentPageCompileOptions);
+      expect(key).toEqual('value');
+    });
+    it('getNodeEventKeyByNodeId', () => {
+      const key = getNodeEventKeyByNodeId('N735', '641a7052cf44f6204642b87b', currentPageCompileOptions);
+      expect(key).toEqual('change');
+    });
+  });
+});
