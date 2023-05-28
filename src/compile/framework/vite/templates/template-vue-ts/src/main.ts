5c74084060d33ffa94c6a8f4d66a2528d1d206af
james <james@dreawer.com>
2023-05-28 15:59:54 +0800

    feat: 引入程序元件包

--- src/compile/framework/vite/templates/template-vue-ts/src/main.ts
+++ src/compile/framework/vite/templates/template-vue-ts/src/main.ts
@@ -3,7 +3,9 @@ import './style.css';
 import App from './App.vue';
 import router from './router';
 import Antd from 'ant-design-vue';
+import RdElement from '@dreawer/robotdev-element';
 import 'ant-design-vue/dist/antd.less';
+import '@dreawer/robotdev-element/dist/style.css';
 import './assets/vendor/a18e4772.js';
 
-createApp(App).use(router).use(Antd).mount('#app');
+createApp(App).use(router).use(Antd).use(RdElement).mount('#app');
