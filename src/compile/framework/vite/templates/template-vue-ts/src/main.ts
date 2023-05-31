71251e657f3c107880e2ea539582cac8310baa84
ethan <ethan@dreawer.com>
2023-05-31 19:00:26 +0800

    refactor: 去掉错误处理

--- src/compile/framework/vite/templates/template-vue-ts/src/main.ts
+++ src/compile/framework/vite/templates/template-vue-ts/src/main.ts
@@ -7,13 +7,13 @@ import 'ant-design-vue/dist/antd.less';
 import '@dreawer/robotdev-element/dist/style.css';
 import './assets/vendor/a18e4772.js';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.config.errorHandler = (err, instance, info) => {
-  new Error(err as any)
-  console.log('instance:', instance)
-  console.log('info:', info)
-  return null
-};
+// app.config.errorHandler = (err, instance, info) => {
+//   new Error(err as any);
+//   console.log('instance:', instance);
+//   console.log('info:', info);
+//   return null;
+// };
 
 app.use(router).use(Antd).use(RdElement).mount('#app');
